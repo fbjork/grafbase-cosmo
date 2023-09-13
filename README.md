cb6859f9a90bc95c2c7adbcbadf176c587c1ae6c
Fredrik Björk <fredrik.bjork@gmail.com>
2023-09-13 09:50:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Grafbase + Cosmo example
+
+# Getting Started
+
+# Install and run Cosmo
+Follow these steps:
+https://github.com/wundergraph/cosmo#getting-started
+
+# Configure the Cosmo data source
+
+Set the following environment variables:
+
+```
+COSMO_API_URL=
+COSMO_API_KEY=
+```
+
+# Run Grafbase
+
+```
+npx grafbase@latest dev
+```
---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,23 @@
+import { config, connector, g } from '@grafbase/sdk'
+
+const cosmo = connector.GraphQL('Cosmo', {
+  url: g.env('COSMO_API_URL'),
+  headers: headers => {
+    headers.set('Authorization', `Bearer ${g.env('COSMO_API_TOKEN')}`)
+  }
+})
+
+g.datasource(cosmo, { namespace: false })
+
+export default config({
+  schema: g,
+  cache: {
+    rules: [
+      {
+        types: ['Query'],
+        maxAge: 60,
+        staleWhileRevalidate: 60
+      }
+    ]
+  }
+})
